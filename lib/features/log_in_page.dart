18d0ac8acb155c988141332a9534ea82f50675ba
Kent Lorenz Daria <kentlorenz.daria@wvsu.edu.ph>
2023-10-02 10:26:57 +0800

    relocated features page

--- lib/features/log_in_page.dart
+++ lib/features/log_in_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class LogInPage extends StatelessWidget {
+  const LogInPage({super.key});
 
   @override
   Widget build(BuildContext context) {
