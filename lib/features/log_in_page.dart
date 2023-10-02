a68ed83327a237786be25cafdf57fefc6b8f5907
Kent Lorenz Daria <kentlorenz.daria@wvsu.edu.ph>
2023-10-02 10:49:53 +0800

    implemented log in page ui

--- lib/features/log_in_page.dart
+++ lib/features/log_in_page.dart
@@ -5,6 +5,39 @@ class LogInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Username'),
+              ),
+              SizedBox(height: 20.0),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Add your login logic here
+                },
+                child: Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    )
+    );
   }
 }
+
+
