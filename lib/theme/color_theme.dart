0a5de3e274cdbbf8f1bfd7a8822e8ac8c0aff488
Kent Lorenz Daria <kentlorenz.daria@wvsu.edu.ph>
2023-10-20 23:02:10 +0800

    added color themes

---
+++ lib/theme/color_theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+const textColor = Color(0xFF010709);
+const backgroundColor = Color.fromARGB(255, 255, 255, 255);
+const primaryColor = Color(0xFF971779);
+const primaryFgColor = Color(0xFFe9f7fc);
+const secondaryColor = Color(0xFFb4e5f4);
+const secondaryFgColor = Color(0xFF010709);
+const accentColor = Color(0xFF99bf1d);
+const accentFgColor = Color(0xFF010709);
+
+const colorScheme1 = ColorScheme(
+  brightness: Brightness.light,
+  background: backgroundColor,
+  onBackground: textColor,
+  primary: primaryColor,
+  onPrimary: primaryFgColor,
+  secondary: secondaryColor,
+  onSecondary: secondaryFgColor,
+  tertiary: accentColor,
+  onTertiary: accentFgColor,
+  surface: backgroundColor,
+  onSurface: textColor,
+  error: Brightness.light == Brightness.light
+      ? Color(0xffB3261E)
+      : Color(0xffF2B8B5),
+  onError: Brightness.light == Brightness.light
+      ? Color(0xffFFFFFF)
+      : Color(0xff601410),
+);
