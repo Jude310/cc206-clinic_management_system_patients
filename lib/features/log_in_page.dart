176189de3df1e267fa196192bbb9134aa0688c93
Kent Lorenz Daria <kentlorenz.daria@wvsu.edu.ph>
2023-10-20 23:03:10 +0800

    changed some colors and added icons and layout

--- lib/features/log_in_page.dart
+++ lib/features/log_in_page.dart
@@ -1,5 +1,7 @@
+import 'package:cc206_clinic_management_website_patients/theme/color_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
 
 class LogInPage extends StatelessWidget {
   const LogInPage({super.key});
@@ -7,71 +9,84 @@ class LogInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          SvgPicture.asset(
-            'assets/logo/logo_1.svg',
-            width: 300,
-          ),
-          const Text(
-            'Patient Login',
-            style: TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SvgPicture.asset(
+              'assets/logo/logo_1.svg',
+              width: 300,
             ),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 20.0),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0),
-            child: TextField(
+            const Text(
+              'Patient Login',
+              
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20.0),
+            TextField(
               decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                ),
+                prefixIcon:
+                    Padding(
+                      padding: EdgeInsets.only(left: 16.0,right: 10.0),
+                      child: Icon(Icons.account_circle, color: colorScheme1.primary),
+                    ),
                 labelText: 'Username',
               ),
             ),
-          ),
-          const SizedBox(height: 20.0),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0),
-            child: TextFormField(
-              decoration: const InputDecoration(
+            const SizedBox(height: 20.0),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                ),
+                prefixIcon: Padding(
+                  padding: EdgeInsets.only(left: 20.0, right: 10.0),
+                  child: Icon(Icons.lock, color: colorScheme1.primary),
+                ),
                 labelText: 'Password',
               ),
               obscureText: true,
             ),
-          ),
-          const SizedBox(height: 20.0),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 45.0),
-            child: ElevatedButton(
-              onPressed: () {
-                // Submit Login function
-              },
-              child: const Text(
-                'Login',
-                style:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-              ),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(
-                    Theme.of(context).colorScheme.primary),
+            const SizedBox(height: 20.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 95.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Submit Login function
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      colorScheme1.tertiary),
+                ),
+                child: const Text(
+                  'Login',
+                  style: TextStyle(
+                      color: accentFgColor , fontWeight: FontWeight.bold),
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 10.0),
-          //make a hyperlink text
+            const SizedBox(height: 10.0),
+            //make a hyperlink text
 
-          const Text(
-            'No account? Cick here to Sign up',
-            style: TextStyle(
-              fontSize: 12.0,
-              decoration: TextDecoration.underline,
+            const Text(
+              'No account? Cick here to Sign up',
+              style: TextStyle(
+                fontSize: 12.0,
+                decoration: TextDecoration.underline,
+              ),
+              textAlign: TextAlign.center,
             ),
-            textAlign: TextAlign.center,
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
