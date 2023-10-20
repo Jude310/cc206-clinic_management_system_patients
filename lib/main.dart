de2a34e4b29812be0bb73feaf50d9113b1f6703b
Kent Lorenz Daria <kentlorenz.daria@wvsu.edu.ph>
2023-10-20 23:02:20 +0800

    imported color theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'features/log_in_page.dart';
+import 'theme/color_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,17 +8,14 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData( 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true, 
-
-      ), 
-      home: LogInPage()
-      );
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: colorScheme1,
+          useMaterial3: true,
+        ),
+        home: LogInPage());
   }
 }
