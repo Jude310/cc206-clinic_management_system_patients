c380a91e31851a9e2c97c3dad32fac18bb6524fb
Kent Lorenz Daria <kentlorenz.daria@wvsu.edu.ph>
2023-10-23 11:10:18 +0800

    refractored the text input field to be form fields

--- lib/features/log_in_page.dart
+++ lib/features/log_in_page.dart
@@ -4,7 +4,8 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 
 class LogInPage extends StatelessWidget {
-  const LogInPage({super.key});
+  LogInPage({super.key});
+  final logInFormKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -15,75 +16,83 @@ class LogInPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            SvgPicture.asset(
-              'assets/logo/logo_1.svg',
-              width: 300,
-            ),
-            const Text(
-              'Patient Login',
-              
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 20.0),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                ),
-                prefixIcon:
-                    Padding(
-                      padding: EdgeInsets.only(left: 16.0,right: 10.0),
-                      child: Icon(Icons.account_circle, color: colorScheme1.primary),
+            Form(
+              key: logInFormKey,
+              child: Column(
+                children: [
+                  SvgPicture.asset(
+                    'assets/logo/logo_1.svg',
+                    width: 300,
+                  ),
+                  const Text(
+                    'Patient Login',
+                    
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
                     ),
-                labelText: 'Username',
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 20.0),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                      ),
+                      prefixIcon:
+                          Padding(
+                            padding: EdgeInsets.only(left: 16.0,right: 10.0),
+                            child: Icon(Icons.account_circle, color: colorScheme1.primary),
+                          ),
+                      labelText: 'Username',
+                    ),
+                  
+                  ),
+                  const SizedBox(height: 20.0),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                      ),
+                      prefixIcon: Padding(
+                        padding: EdgeInsets.only(left: 20.0, right: 10.0),
+                        child: Icon(Icons.vpn_key, color: colorScheme1.primary),
+                      ),
+                      labelText: 'Password',
+                    ),
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 20.0),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 95.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Submit Login function
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            colorScheme1.tertiary),
+                      ),
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(
+                            color: accentFgColor , fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  //make a hyperlink text
+                      
+                  const Text(
+                    'No account? Cick here to Sign up',
+                    style: TextStyle(
+                      fontSize: 12.0,
+                      decoration: TextDecoration.underline,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
               ),
-            ),
-            const SizedBox(height: 20.0),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                ),
-                prefixIcon: Padding(
-                  padding: EdgeInsets.only(left: 20.0, right: 10.0),
-                  child: Icon(Icons.lock, color: colorScheme1.primary),
-                ),
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 20.0),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 95.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Submit Login function
-                },
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      colorScheme1.tertiary),
-                ),
-                child: const Text(
-                  'Login',
-                  style: TextStyle(
-                      color: accentFgColor , fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
-            const SizedBox(height: 10.0),
-            //make a hyperlink text
-
-            const Text(
-              'No account? Cick here to Sign up',
-              style: TextStyle(
-                fontSize: 12.0,
-                decoration: TextDecoration.underline,
-              ),
-              textAlign: TextAlign.center,
             ),
           ],
         ),
