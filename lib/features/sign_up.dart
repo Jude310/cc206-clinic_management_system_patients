8d12762389ffffe2729da36c1d7df219a729227c
Kent Lorenz Daria <kentlorenz.daria@wvsu.edu.ph>
2023-11-12 15:06:06 +0800

    add bg image

--- lib/features/sign_up.dart
+++ lib/features/sign_up.dart
@@ -1,86 +1,132 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:cc206_clinic_management_website_patients/theme/color_theme.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class SignUpApp extends StatelessWidget {
   void _logIn() {}
   void _signUp() {}
-  void _signInWithGoogle() {}
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text("Sign Up Page")),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Name",
-              ),
+    return Stack(
+      children: [
+        SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Image.asset(
+              'assets/images/LoginSignUp_BG (1) 1.png',
+              // fit: BoxFit.cover,
+            )),
+        Scaffold(
+          body: Padding( 
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 16.0),
+                Row(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white.withOpacity(0.8),
+                        contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        ),
+                        prefixIcon: Padding(
+                          padding: EdgeInsets.only(left: 16.0, right: 10.0),
+                          child: Icon(Icons.account_circle,
+                              color: colorScheme1.primary),
+                        ),
+                        labelText: 'First Name',
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white.withOpacity(0.8),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                        ),
+                        prefixIcon: Padding(
+                          padding: EdgeInsets.only(left: 16.0, right: 10.0),
+                          child: Icon(Icons.account_circle,
+                              color: colorScheme1.primary),
+                        ),
+                        labelText: 'Last Name',
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16.0),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white.withOpacity(0.8),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                    ),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(left: 16.0, right: 10.0),
+                      child: Icon(Icons.account_circle,
+                          color: colorScheme1.primary),
+                    ),
+                    labelText: 'Email',
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white.withOpacity(0.8),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                    ),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(left: 16.0, right: 10.0),
+                      child: Icon(Icons.account_circle,
+                          color: colorScheme1.primary),
+                    ),
+                    labelText: 'Password',
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white.withOpacity(0.8),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                    ),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(left: 16.0, right: 10.0),
+                      child: Icon(Icons.account_circle,
+                          color: colorScheme1.primary),
+                    ),
+                    labelText: 'Birthdate',
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                ElevatedButton(
+                    onPressed: _signUp,
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF43C6AC),
+                        foregroundColor: Colors.black),
+                    child: Text("Sign Up")),
+                SizedBox(height: 16.0),
+                ElevatedButton(
+                    onPressed: _logIn,
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF43C6AC),
+                        foregroundColor: Colors.black),
+                    child: Text("Login")),
+                SizedBox(height: 16.0),
+              ],
             ),
-            SizedBox(height: 16.0),
-            TextField(
-              keyboardType: TextInputType.datetime,
-              decoration: InputDecoration(
-                labelText: "Birthdate",
-              ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: "Email",
-              ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              keyboardType: TextInputType.phone,
-              decoration: InputDecoration(
-                labelText: "Phone",
-              ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: "Password",
-              ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: "Confirm Password",
-              ),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-                onPressed: _signUp,
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF43C6AC),
-                    foregroundColor: Colors.black),
-                child: Text("Sign Up")),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-                onPressed: _logIn,
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF43C6AC),
-                    foregroundColor: Colors.black),
-                child: Text("Login")),
-            SizedBox(height: 16.0),
-            OutlinedButton(
-                onPressed: _signInWithGoogle,
-                style: OutlinedButton.styleFrom(foregroundColor: Colors.black),
-                child: Text("Sign In with Google")),
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
