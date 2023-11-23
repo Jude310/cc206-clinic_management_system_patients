3b192ac5bee6da906e5734602b40074c5c52ac1a
Kent Lorenz Daria <kentlorenz.daria@wvsu.edu.ph>
2023-11-23 13:47:04 +0800

    moved sign_up to folder for directory management

--- lib/features/sign_up/sign_up.dart
+++ lib/features/sign_up/sign_up.dart
@@ -2,6 +2,7 @@
 
 import 'package:cc206_clinic_management_website_patients/theme/color_theme.dart';
 import 'package:flutter/material.dart';
+import '../components/sign_up_form_input_widget.dart';
 
 class SignUpApp extends StatelessWidget {
   @override
@@ -21,7 +22,7 @@ class SignUpApp extends StatelessWidget {
             child: Center(
                 child: SingleChildScrollView(
               child: Column(
-                children: [
+                children: const[
                   Text(
                     'Let\'s get you set up!',
                     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
@@ -186,104 +187,5 @@ class _SignUpFormState extends State<SignUpForm> {
         ),
       );
     });
-  }
-}
-
-class SignUpFormInputWidget extends StatefulWidget {
-  final String label;
-  final String? hintText;
-  final String? Function(String?)? validator;
-  final void Function(String)? onChanged;
-  final AutovalidateMode? autovalidateMode;
-  final IconData? prefixIcon;
-  final TextEditingController? controller;
-  final void Function()? onTap;
-  final bool readOnly;
-  const SignUpFormInputWidget({
-    this.label = '',
-    this.hintText,
-    this.validator,
-    this.onChanged,
-    this.autovalidateMode,
-    this.prefixIcon,
-    this.controller,
-    this.onTap,
-    this.readOnly = false,
-    super.key,
-  });
-
-  @override
-  State<SignUpFormInputWidget> createState() => _SignUpFormInputWidgetState();
-}
-
-class _SignUpFormInputWidgetState extends State<SignUpFormInputWidget> {
-  final FocusNode _focusNode = FocusNode();
-  bool _hasFocus = false;
-  @override
-  void initState() {
-    super.initState();
-    _focusNode.addListener(() {
-      setState(() {
-        _hasFocus = _focusNode.hasFocus;
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    // Clean up the focus node when the Form is disposed.
-    _focusNode.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          widget.label,
-          textAlign: TextAlign.start,
-          style: TextStyle(
-            color:
-                !_hasFocus ? colorScheme1.onBackground : colorScheme1.primary,
-            fontSize: 14.0,
-            fontWeight: !_hasFocus ? FontWeight.w500 : FontWeight.w600,
-          ),
-          overflow: TextOverflow.fade,
-        ),
-        Material(
-          color: Colors.transparent,
-          borderRadius: BorderRadius.all(Radius.circular(20.0)),
-          elevation: 10.0,
-          child: TextFormField(
-            readOnly: widget.readOnly,
-            onTap: widget.onTap,
-            focusNode: _focusNode,
-            controller: widget.controller,
-            validator: widget.validator,
-            autovalidateMode: widget.autovalidateMode,
-            onChanged: widget.onChanged,
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: Colors.white.withOpacity(0.95),
-              contentPadding:
-                  EdgeInsets.symmetric(vertical: 15.0, horizontal: 20),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(20.0)),
-              ),
-              prefixIcon: widget.prefixIcon != null
-                  ? Padding(
-                      padding: EdgeInsets.only(left: 16.0, right: 10.0),
-                      child:
-                          Icon(widget.prefixIcon, color: colorScheme1.primary),
-                    )
-                  : null,
-              hintText: widget.hintText,
-            ),
-          ),
-        ),
-      ],
-    );
   }
 }
