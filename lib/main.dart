2c7c4dbad38d99615800de699bd56951228a210d
Jude310 <99381703+Jude310@users.noreply.github.com>
2023-11-24 23:38:02 +0800

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:cc206_clinic_management_website_patients/features/sign_up.dart';
+import 'package:cc206_clinic_management_website_patients/theme/color_theme.dart';
 import 'package:flutter/material.dart';
-import 'theme/color_theme.dart';
+import 'features/sign_up/sign_up.dart';
+import 'features/log_in/log_in_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,17 +9,44 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hinosa Sign Up Page',
+      title: 'Hinosa Log In Page',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'Montserrat',
         colorScheme: colorScheme1,
         useMaterial3: true,
+        // Add your theme configuration here
       ),
-      home: SignUpApp(),
+      home: LogInPage(),
+    );
+  }
+}
+
+class AppNavigator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: GlobalKey<NavigatorState>(),
+      onGenerateRoute: (settings) {
+        late Widget page;
+        switch (settings.name) {
+          case '/signup':
+            page = SignUp();
+            break;
+          case '/login':
+            page = LogInPage(); // Add your login page
+            break;
+          default:
+            // If the route is not found, navigate to the signup page by default
+            page = SignUp();
+        }
+
+        return MaterialPageRoute(builder: (context) => page);
+      },
     );
   }
 }
