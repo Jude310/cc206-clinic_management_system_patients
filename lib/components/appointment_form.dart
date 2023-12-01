f9fa9220df71318e24122f9e9d45af76de8ba528
Kent Lorenz Daria <kentlorenz.daria@wvsu.edu.ph>
2023-12-01 16:24:00 +0800

    esit design for appooint ment form

--- lib/components/appointment_form.dart
+++ lib/components/appointment_form.dart
@@ -1,3 +1,4 @@
+import 'package:cc206_clinic_management_website_patients/theme/color_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -8,7 +9,8 @@ class AppointmentFormPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Request Appointment'),
+        title: const Text('Request Appointment',
+            style: TextStyle(fontWeight: FontWeight.bold)),
       ),
       body: AppointmentForm(),
     );
@@ -42,50 +44,109 @@ class _AppointmentFormState extends State<AppointmentForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          TextFormField(
-            controller: _reasonController,
-            decoration: InputDecoration(
-              labelText: 'Reason of Visit',
-              border: OutlineInputBorder(),
-              contentPadding: const EdgeInsets.all(16.0),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Choose a Date for your Appointment',
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
             ),
-          ),
-          const SizedBox(height: 16.0),
-          Row(
-            children: [
-              const Icon(Icons.calendar_today),
-              const SizedBox(width: 8.0),
-              Text(
-                'Selected Date: ${_dateFormat.format(_selectedDate)}',
-                style: TextStyle(fontSize: 16.0),
+            Card(
+              color: colorScheme1.primary,
+              child: Stack(
+                children: [
+                  Positioned(
+                      right: 0,
+                      top: -10,
+                      child: Opacity(
+                          opacity: 0.4,
+                          child: Image.asset(
+                            'assets/images/calendar.png',
+                            width: 150,
+                          ))),
+                  SizedBox(
+                      height: 105,
+                      width: MediaQuery.of(context).size.width - 20,
+                      child: Padding(
+                        padding: const EdgeInsets.all(14.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Selected Date:',
+                                    style: TextStyle(
+                                        fontSize: 14.0,
+                                        fontWeight: FontWeight.w400,
+                                        color: colorScheme1.onPrimary),
+                                  ),
+                                  Text(
+                                    _dateFormat.format(_selectedDate),
+                                    style: TextStyle(
+                                        fontSize: 25.0,
+                                        fontWeight: FontWeight.w600,
+                                        color: colorScheme1.onPrimary),
+                                  ),
+                                ]),
+                            TextButton(
+                              onPressed: () => _selectDate(context),
+                              style: TextButton.styleFrom(
+                                backgroundColor:
+                                    colorScheme1.onPrimary.withOpacity(0.8),
+                                primary: colorScheme1.onSecondary,
+                                padding: const EdgeInsets.all(5.0),
+                              ),
+                              child: Icon(
+                                Icons.edit_calendar_outlined,
+                                color: colorScheme1.primary,
+                              ),
+                            ),
+                          ],
+                        ),
+                      )),
+                ],
               ),
-              const Spacer(),
-              TextButton(
-                onPressed: () => _selectDate(context),
-                child: const Text(
-                  'Choose Date',
-                  style: TextStyle(color: Colors.blue),
+            ),
+            const SizedBox(height: 16.0),
+            const Text(
+              'Reason for Appointment',
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            TextFormField(
+              maxLines: 6,
+              controller: _reasonController,
+              decoration: InputDecoration(
+                hintText:
+                    'Indicate any reason for your appointment e.g. fever for 3 days, experiencing pain in my left arm, etc.',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16.0)),
+                contentPadding: const EdgeInsets.all(12.0),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            Align(
+              alignment: Alignment.centerRight,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Implement appointment submission logic here
+                  // You can use _reasonController.text and _selectedDate to get the form data
+                },
+                child: const Text('Submit', style: TextStyle(fontWeight: FontWeight.bold)),
+                style: ElevatedButton.styleFrom(
+                  primary: colorScheme1.primary,
+                  foregroundColor: colorScheme1.onPrimary,
+                  padding: const EdgeInsets.all(16.0),
                 ),
               ),
-            ],
-          ),
-          const SizedBox(height: 16.0),
-          ElevatedButton(
-            onPressed: () {
-              // Implement appointment submission logic here
-              // You can use _reasonController.text and _selectedDate to get the form data
-            },
-            child: const Text('Submit'),
-            style: ElevatedButton.styleFrom(
-              padding: const EdgeInsets.all(16.0),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
