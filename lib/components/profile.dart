43ee7e4024cfe5e70bf1f6d654e6cc4105e178b8
Kent Lorenz Daria <kentlorenz.daria@wvsu.edu.ph>
2023-12-01 16:24:31 +0800

    added paddiing

--- lib/components/profile.dart
+++ lib/components/profile.dart
@@ -13,65 +13,56 @@ class MyProfile extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 20),
-            Center(
-              child: CircleAvatar(
-                radius: 60.0,
-                backgroundImage: NetworkImage('https://picsum.photos/seed/377/600'),
-              ),
-            ),
-            Center(
-              child: Text(
-                'Isaiah Louis Emmanuel S. Yee',
-                style: TextStyle(fontSize: 24),
-              ),
-            ),
-            const SizedBox(height: 10),
-            Center(
-              child: Text(
-                '@$username',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.grey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal:16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20),
+              Center(
+                child: CircleAvatar(
+                  radius: 60.0,
+                  backgroundImage: NetworkImage('https://picsum.photos/seed/377/600'),
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            ProfileSection(
-              title: 'Email',
-              data: 'isaiahlouis.yee@wvsu.edu.ph',
-              icon: Icons.email,
-            ),
-            ProfileSection(title: 'Birthdate', data: birthdate, icon: Icons.cake),
-            ProfileSection(title: 'Sex', data: sex, icon: Icons.person_outline),
-            ProfileSection(
-              title: 'Total Appointments',
-              data: totalAppointments.toString(),
-              icon: Icons.event,
-            ),
-            ProfileSection(
-              title: 'Account Created On',
-              data: _formatDate(accountCreationDate),
-              icon: Icons.calendar_today,
-            ),
-            const SizedBox(height: 20),
-            Center(
-              child: Text(
-                'My Biography',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              Center(
+                child: Text(
+                  'Isaiah Louis Emmanuel S. Yee',
+                  style: TextStyle(fontSize: 24),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Text(
-                ' I am a computer science student at West Visayas State University (WVSU). At 20 years old, I have had a learning journey with its own pace, marked by moments of shyness and a strong desire to learn. My academic path has had its challenges, and I sometimes felt like I am catching up slowly in the ever-changing world of computer science. But my determination to learn remains unwavering. While I would not call myself an exceptional coder, I am eager to learn and improve.',
-                style: TextStyle(fontSize: 16), textAlign: TextAlign.center,
+              const SizedBox(height: 10),
+              Center(
+                child: Text(
+                  '@$username',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.grey,
+                  ),
+                ),
               ),
-            ),
-          ],
+              const SizedBox(height: 20),
+              ProfileSection(
+                title: 'Email',
+                data: 'isaiahlouis.yee@wvsu.edu.ph',
+                icon: Icons.email,
+              ),
+              ProfileSection(title: 'Birthdate', data: birthdate, icon: Icons.cake),
+              ProfileSection(title: 'Sex', data: sex, icon: Icons.person_outline),
+              ProfileSection(
+                title: 'Total Appointments',
+                data: totalAppointments.toString(),
+                icon: Icons.event,
+              ),
+              ProfileSection(
+                title: 'Account Created On',
+                data: _formatDate(accountCreationDate),
+                icon: Icons.calendar_today,
+              ),
+              const SizedBox(height: 20),
+              
+            ],
+          ),
         ),
       ),
     );
