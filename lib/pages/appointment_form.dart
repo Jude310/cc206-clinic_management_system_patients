6c6b4fb0b685fd43e4c9648bab510d4511b3ce96
Angel Jude Diones <angeljude.diones@wvsu.edu.ph>
2023-12-03 14:05:44 +0800

    Updated appointment_form

--- lib/pages/appointment_form.dart
+++ lib/pages/appointment_form.dart
@@ -1,5 +1,6 @@
-import 'package:cc206_clinic_management_website_patients/theme/color_theme.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 
 class AppointmentFormPage extends StatelessWidget {
@@ -42,6 +43,36 @@ class _AppointmentFormState extends State<AppointmentForm> {
     }
   }
 
+  Future<void> _sendAppointmentRequest() async {
+    final Map<String, dynamic> appointmentData = {
+      'subject': 'Appointment Request',
+      'text':
+          'Name: John Cena\nAppointment Date: ${_dateFormat.format(_selectedDate)}\nReason: ${_reasonController.text}',
+    };
+
+    final String jsonData = jsonEncode(appointmentData);
+
+    try {
+      final http.Response response = await http.post(
+        Uri.parse('http://localhost:3200/email/send-dynamic-email'),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonData,
+      );
+
+      if (response.statusCode == 200) {
+        print('Appointment request sent successfully!');
+      } else {
+        print(
+            'Failed to send appointment request. Status Code: ${response.statusCode}');
+        print('Response Body: ${response.body}');
+      }
+    } catch (error) {
+      print('Error sending appointment request: $error');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -55,60 +86,71 @@ class _AppointmentFormState extends State<AppointmentForm> {
               style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
             ),
             Card(
-              color: colorScheme1.primary,
+              color: Colors.blue, // Change this to your colorScheme1.primary
               child: Stack(
                 children: [
                   Positioned(
-                      right: 0,
-                      top: -10,
-                      child: Opacity(
-                          opacity: 0.4,
-                          child: Image.asset(
-                            'assets/images/calendar.png',
-                            width: 150,
-                          ))),
+                    right: 0,
+                    top: -10,
+                    child: Opacity(
+                      opacity: 0.4,
+                      child: Image.asset(
+                        'assets/images/calendar.png',
+                        width: 150,
+                      ),
+                    ),
+                  ),
                   SizedBox(
-                      height: 105,
-                      width: MediaQuery.of(context).size.width - 20,
-                      child: Padding(
-                        padding: const EdgeInsets.all(14.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    'Selected Date:',
-                                    style: TextStyle(
-                                        fontSize: 14.0,
-                                        fontWeight: FontWeight.w400,
-                                        color: colorScheme1.onPrimary),
-                                  ),
-                                  Text(
-                                    _dateFormat.format(_selectedDate),
-                                    style: TextStyle(
-                                        fontSize: 25.0,
-                                        fontWeight: FontWeight.w600,
-                                        color: colorScheme1.onPrimary),
-                                  ),
-                                ]),
-                            TextButton(
-                              onPressed: () => _selectDate(context),
-                              style: TextButton.styleFrom(
-                                backgroundColor: colorScheme1.tertiary,
-                                primary: colorScheme1.onSecondary,
-                                padding: const EdgeInsets.all(5.0),
+                    height: 105,
+                    width: MediaQuery.of(context).size.width - 20,
+                    child: Padding(
+                      padding: const EdgeInsets.all(14.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Selected Date:',
+                                style: TextStyle(
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors
+                                      .white, // Change this to your colorScheme1.onPrimary
+                                ),
                               ),
-                              child: Icon(
-                                Icons.edit_calendar_outlined,
-                                color: colorScheme1.primary,
+                              Text(
+                                _dateFormat.format(_selectedDate),
+                                style: TextStyle(
+                                  fontSize: 25.0,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors
+                                      .white, // Change this to your colorScheme1.onPrimary
+                                ),
                               ),
+                            ],
+                          ),
+                          TextButton(
+                            onPressed: () => _selectDate(context),
+                            style: TextButton.styleFrom(
+                              backgroundColor: Colors
+                                  .orange, // Change this to your colorScheme1.tertiary
+                              primary: Colors
+                                  .white, // Change this to your colorScheme1.onSecondary
+                              padding: const EdgeInsets.all(5.0),
                             ),
-                          ],
-                        ),
-                      )),
+                            child: Icon(
+                              Icons.edit_calendar_outlined,
+                              color: Colors
+                                  .blue, // Change this to your colorScheme1.primary
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -121,8 +163,7 @@ class _AppointmentFormState extends State<AppointmentForm> {
               maxLines: 6,
               controller: _reasonController,
               decoration: InputDecoration(
-                hintText:
-                    'Indicate any reason for your appointment e.g. fever for 3 days, experiencing pain in my left arm, etc.',
+                hintText: 'Indicate any reason for your appointment...',
                 border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(16.0)),
                 contentPadding: const EdgeInsets.all(12.0),
@@ -132,16 +173,16 @@ class _AppointmentFormState extends State<AppointmentForm> {
             Align(
               alignment: Alignment.centerRight,
               child: ElevatedButton(
-                onPressed: () {
+                onPressed: () async {
+                  await _sendAppointmentRequest();
                   Navigator.pop(context);
-                  // Implement appointment submission logic here
-                  // You can use _reasonController.text and _selectedDate to get the form data
                 },
                 child: const Text('Submit',
                     style: TextStyle(fontWeight: FontWeight.bold)),
                 style: ElevatedButton.styleFrom(
-                  primary: colorScheme1.primary,
-                  foregroundColor: colorScheme1.onPrimary,
+                  primary:
+                      Colors.blue, // Change this to your colorScheme1.primary
+                  foregroundColor: Colors.white,
                   padding: const EdgeInsets.all(16.0),
                 ),
               ),
