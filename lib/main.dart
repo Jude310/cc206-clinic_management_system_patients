e5d319cbc96be3b740905600fa5c798727c76a3e
Kent Lorenz Daria <kentlorenz.daria@wvsu.edu.ph>
2023-12-03 14:29:16 +0800

    removed unnecessary imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:cc206_clinic_management_website_patients/theme/color_theme.dart';
 import 'package:flutter/material.dart';
-import 'features/sign_up/sign_up.dart';
-import 'features/log_in/log_in_page.dart';
-import 'theme/color_theme.dart';
 
+import 'features/log_in/log_in_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,28 +26,28 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class AppNavigator extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      navigatorKey: GlobalKey<NavigatorState>(),
-      onGenerateRoute: (settings) {
-        late Widget page;
-        switch (settings.name) {
-          case '/signup':
-            page = SignUp();
-            break;
-          case '/login':
-            page = LogInPage(); // Add your login page
-            break;
-          default:
-            // If the route is not found, navigate to the signup page by default
-            page = SignUp();
-        }
+// class AppNavigator extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       navigatorKey: GlobalKey<NavigatorState>(),
+//       onGenerateRoute: (settings) {
+//         late Widget page;
+//         switch (settings.name) {
+//           case '/signup':
+//             page = SignUp();
+//             break;
+//           case '/login':
+//             page = LogInPage(); // Add your login page
+//             break;
+//           default:
+//             // If the route is not found, navigate to the signup page by default
+//             page = SignUp();
+//         }
 
-        return MaterialPageRoute(builder: (context) => page);
-      },
-    );
+//         return MaterialPageRoute(builder: (context) => page);
+//       },
+//     );
 
-  }
-}
+//   }
+// }
